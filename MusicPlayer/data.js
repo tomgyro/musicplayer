114d20a2a80183f2039347a162a3327fccf0f52f
tomgyro <tomasgpineyro@gmail.com>
2024-07-14 00:44:48 -0300

    primer push

---
+++ MusicPlayer/data.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name: 'Promesas Sobre El Bidet',
+        path: 'Musics/Charly García - Promesas Sobre El Bidet.mp3',
+        artist: 'Charly García',
+        cover: 'Images/pianoBar.jpg'
+    },
+    {
+        name: 'Idioteque',
+        path: 'Musics/Idioteque-radiohead.mp3',
+        artist: 'Radiohead',
+        cover: 'Images/Kid.jpg'
+    },
+    {
+        name: 'By The Way',
+        path: 'Musics/Red Hot Chili Peppers - By The Way.mp3',
+        artist: 'Red Hot Chili Peppers',
+        cover: 'Images/bytheway.jpg'
+    }
+]
